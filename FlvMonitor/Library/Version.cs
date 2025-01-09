ade887a2984b7413eb41e8db46da8f52a69e9755
hotkidfamily <hotkidfamily@gmail.com>
2025-01-09 17:01:24 +0800

    bump version

--- FlvMonitor/Library/Version.cs
+++ FlvMonitor/Library/Version.cs
@@ -4,7 +4,7 @@ namespace FlvMonitor.Library
 {
     public static class VersionInfo
     {
-        public const string AssemblyVersion = "0.2.0.0";
+        public const string AssemblyVersion = "0.3.0.0";
         public const string CopyrightYears = "2024 ~ 2025";
         public const string Website = "http://github.com/hotkidfamily";
         public const string Authors = "hotkidfamily";
